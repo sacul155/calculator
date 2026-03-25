dc5741277e49532e23ae26c68f5ff3c6051a6500
sacul155 <cr1sram1rez107@gmail.com>
2026-03-24 21:24:58 -0400

    HTML and JS files created HTML file created with boilerplate and link to js file.  JavaScript file created.  Added functions for addition, subtraction, multiplication, and division.

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(6, 7));
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+console.log(subtractNumbers(6, 7));
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+console.log(multiplyNumbers(6, 7));
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+console.log(divideNumbers(6, 7));
