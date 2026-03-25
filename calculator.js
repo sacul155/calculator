bdc6256b5f8ccbf174b8c4a22a62412facecf16f
sacul155 <cr1sram1rez107@gmail.com>
2026-03-24 21:51:32 -0400

    Operation variables and function added Created three variables for holding two numbers and the operation performed between them. Created a new function called operator that reads the three variables and performs corresponding arithmetic function.

--- calculator.js
+++ calculator.js
@@ -13,7 +13,18 @@ const calculator = {
     }
 }
 
-console.log(calculator.add(20, 5));
-console.log(calculator.subtract(20, 5));
-console.log(calculator.multiply(20, 5));
-console.log(calculator.divide(20, 5));
+// console.log(calculator.add(20, 5));
+// console.log(calculator.subtract(20, 5));
+// console.log(calculator.multiply(20, 5));
+// console.log(calculator.divide(20, 5));
+
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+const operation = {
+    "+": (a, b) => calculator.add(a, b),
+    "-": (a, b) => calculator.subtract(a, b),
+    "*": (a, b) => calculator.multiply(a, b),
+    "/": (a, b) => calculator.divide(a, b)
+};
